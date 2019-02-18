b52ff189fc2ecfd47fdaaa3dc6dd9d4bea762aa9
BurningBlaze <pmdblaze@tutanota.com>
2019-02-18 15:17:04 -0500

    Use max npcs for rdungeon editor

--- Client/Windows/Editors/winRDungeonPanel.cs
+++ Client/Windows/Editors/winRDungeonPanel.cs
@@ -2347,7 +2347,7 @@ namespace Client.Logic.Windows.Editors
                 //NumericUpDown nudNpcNum;
                 nudNpcNum = new NumericUpDown("nudNpcNum");
                 nudNpcNum.Minimum = 1;
-                nudNpcNum.Maximum = MaxInfo.MaxItems;
+                nudNpcNum.Maximum = MaxInfo.MaxNpcs;
                 nudNpcNum.Size = new Size(80, 20);
                 nudNpcNum.Location = new Point(50, 74);
                 nudNpcNum.Font = Graphics.FontManager.LoadFont("tahoma", 10);
